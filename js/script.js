f68a355757d40ce75bfe274664b1e34e5c1af6ad
raogpip <vitalik.fedorenko.2018@gmail.com>
2022-11-21 21:24:28 +0100

    Slider fixes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-"use strict";
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 10,
   autoplay: {
@@ -8,8 +7,22 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".my-swiper-pagination",
     clickable: true,
-    renderBullet: function (index, className) {
+    renderBullet: function (className) {
       return '<span class="' + className + '">' + "</span>";
     },
   },
 });
+// var swiper2 = new Swiper(".mySwiper2", {
+//   spaceBetween: 10,
+//   autoplay: {
+//     delay: 4000,
+//   },
+//   speed: 2800,
+//   pagination: {
+//     el: ".my-swiper-pagination",
+//     clickable: true,
+//     renderBullet: function (index, className) {
+//       return '<span class="' + className + '">' + "</span>";
+//     },
+//   },
+// });
